139ce0a12404134a0041c9a95ddfb0c4dae41049
qmafog <qmafog6@gmail.com>
2023-07-08 18:41:57 +0300

    done

--- BubbleSort/Sorter.cs
+++ BubbleSort/Sorter.cs
@@ -10,8 +10,34 @@ namespace BubbleSort
         /// </summary>
         public static void BubbleSort(this int[]? array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
         }
 
         /// <summary>
@@ -19,8 +45,37 @@ namespace BubbleSort
         /// </summary>
         public static void RecursiveBubbleSort(this int[]? array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            RecursiveBubbleSort(array, array.Length);
+        }
+
+        public static void RecursiveBubbleSort(this int[]? array, int length)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (length <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (array[i].CompareTo(array[i + 1]) > 0)
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+
+            RecursiveBubbleSort(array, length - 1);
         }
     }
 }
